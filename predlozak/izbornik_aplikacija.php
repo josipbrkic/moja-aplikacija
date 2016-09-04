c0ed84ef55e9da09b589c772950e8afb43618318
Josip Brkic <brkic.josip@gmail.com>
2016-09-04 22:55:23 +0200

    Add files via upload

--- predlozak/izbornik_aplikacija.php
+++ predlozak/izbornik_aplikacija.php
@@ -1,4 +1,4 @@
-<nav id="mainNav" class="navbar mojBoja navbar-default">
+<nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
         <div class="container-fluid">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
@@ -9,6 +9,7 @@
                     <span class="icon-bar"></span>
                 </button>
                 <a class="navbar-brand page-scroll mojBoja" href="index.php">Naslovnica</a>
+                
             </div>
              
             
@@ -16,10 +17,21 @@
 
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                  <li>
-                        <a href="#">Aplikacija</a>
+                <ul class="nav navbar-nav navbar-right mojaBoja">
+       				<li>
+       					<?php if(isset($_SESSION["kupac"])): ?>
+	    						<a class="button" href="<?php echo $putanjaAPP; ?>odjava.php">Odjavi 
+	    							<?php echo $_SESSION["kupac"]->ime . " " . $_SESSION["kupac"]->prezime; ?>
+	    						</a>
+	    				<?php else: ?>
+	    						
+	    			
+       				</li>
+	    			
+                  	<li>
+                        <a class="page-scroll" href="autorizacija.php">Prijava</a>
                     </li>
+                    		<?php endif; ?>
                     
                 </ul>
             </div>
